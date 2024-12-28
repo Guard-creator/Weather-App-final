c37f134747d4b135e89056ec6b88b9a5fdf08b4e
Basit Shaikh <shaikhbasit279@gmail.com>
2024-12-29 01:31:10 +0500

    declaring Variables and function

---
+++ scripts.js
@@ -0,0 +1,38 @@
+/*
+    <div class="weather-emoji"><img src="Weather Icons/for sunrise/01d@2x.png"></div>
+    <div class="temp-display">5°C</div>
+    <div class="desc-display">overcast clouds</div>
+    <div class="city-display">London</div>
+    <div class="atmosphere-dev">
+      <div class="humidity-display">humidity: 94%</div>
+      <div class="windspeed-display">wind Speed: 3 mph</div>
+    </div>
+*/
+
+const cityInput = document.querySelector('.city-input');
+const weatherCont = document.querySelector('.weather-details');
+
+let city = '';
+
+document.querySelector('.search-btn')
+  .addEventListener('click', () => {
+
+      
+
+  })
+
+async function getWeatherData() {
+
+}
+
+function displayWeatherData() {
+
+}
+
+function getWeatherImg() {
+
+}
+
+function displayError() {
+
+}
